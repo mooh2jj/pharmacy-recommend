7338ac4ea5d689e85d66c16ebd357381b5a87c77
seonggon do <mooh2jj@naver.com>
2025-04-14 11:25:49 +0900

    Add documentation for Base62 encoding in Base62Service

--- src/main/java/com/dsg/pharmacyrecommend/direction/service/Base62Service.java
+++ src/main/java/com/dsg/pharmacyrecommend/direction/service/Base62Service.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 public class Base62Service {
 
     // https://github.com/seruco/base62
+    /**
+     * base64는 "=" 등 URL 예약어이기 때문에 부적절
+     * base62 -> Base62는 Base64와 유사하지만, 특수 문자를 제외하고 알파벳 대문자(A-Z), 소문자(a-z), 숫자(0-9)만 사용합니다.
+     * 이는 URL이나 파일 이름처럼 특수 문자를 사용할 수 없는 환경에서 유용
+     */
     private static final Base62 base62Instance = Base62.createInstance();
 
     public String encodeDirectionId(Long directionId) {
